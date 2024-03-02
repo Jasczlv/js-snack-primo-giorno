97da51d83c5de7ba1e099cf2c3ac4bb42a11cc39
Jasczlv <ianis.cozlov@gmail.com>
2024-03-02 13:20:31 +0100

    ADD javascript

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// ES 1
+
+let number = 100; //number
+console.log(number); //number
+
+
+// ES 2
+
+const PI = 3.14; //number
+console.log(PI); //number
+
+
+// ES 2/B
+
+// const PI = 3.1416;
+// console.log(PI) CONST CAMBIATA = ERRORE
+
+
+// ES 2/C
+
+let radius = 8; //number
+let circle = radius * 2 * PI; //number
+console.log(circle); //number
+
+
+// ES 3
+
+let name = 'Carlo'; //string
+let name = 'Marco'; //string
+console.log(name); //string
